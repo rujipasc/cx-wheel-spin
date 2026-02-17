72b819a2f42a7190f79f4245a1d2c759282a9c04
rujipas.c <rujipas.jayz@gmail.com>
2026-02-17 17:56:11 +0700

    build(wails): rename app to cx-wheel-spin and set app icon

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "cx-wheel-of-name-native",
+		Title:  "cx-wheel-spin",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
